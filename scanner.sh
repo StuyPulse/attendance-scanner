32aaea43838207aa45da54084115dc90f7b50e3b
elc1798 <subliminalmau5@gmail.com>
2016-01-05 17:47:37 -0500

    Resolve issue with osis numbers

--- scanner.sh
+++ scanner.sh
@@ -250,6 +250,98 @@ function upload_attendance_from_log() {
     done < $1
 }
 
+# Format attendance by removing dates we don't want, and converting csv to ods
+function format_attendance() {
+    # Number to Month
+    case $1 in
+    [1-9]|1[0-2])
+        date=$(date -d "$1/01" +%B) ;;
+    *)
+        printf "${RED}Invalid month${RESET}"
+        return ;;
+    esac
+
+    dump_csv
+    file="$date-Attendance.csv"
+
+    head=$(head -n 1 "$OUTPUT_FILE.csv")
+    found=0
+    first=0
+    last=0
+    IFS=',' read -r -a split <<< "$head"
+
+    for element in "${split[@]}"; do
+        if [[ $element =~ ^$1/[0-9]+/[0-9]{4}$ ]]; then
+            found=1
+        else
+            if [[ $found == 1 ]]; then # We've finished going through meetings of the desired month
+                break
+            fi
+        fi
+
+        if [[ $found == 0 ]]; then # Only increment until we've found the first meeting of the month
+            first=$((first+1))
+        fi
+        last=$((last+1))
+    done
+    first=$((first+1)) # Correct offset
+
+    if [[ $first -gt $last ]]; then
+        printf "${RED}No data for that month.${RESET}\n"
+        return
+    fi
+    cat OUT.csv | cut -d, -f-2,$first-$last > "$file"
+
+    # Remove junk id's, including ones not linked to any names on Team Manager
+    sed -i -e "/[0-9]\{9\},,/d" "$file"
+    sed -i -e "/000000000,/d" "$file"
+    printf "${GREEN}Data formatted to $file${RESET}\n"
+
+    if [[ ! $(command -v csv2ods) ]]; then
+        printf "${YELLOW}csv2ods not detected. Attempting installation...${RESET}\n"
+        sudo apt-get install python-odf-tools
+        if [[ ! $(command -v csv2ods) ]]; then
+            printf "${RED}csv2ods still not detected! Aborting!${RESET}\n"
+        fi
+    fi
+    printf "${YELLOW}Converting from csv to ods...${RESET}\n"
+    csv2ods -i "$date-Attendance.csv" -o "$date-Attendance.ods"
+    printf "${GREEN}$date-Attendance.csv converted to $date-Attendance.ods${RESET}\n"
+
+    echo -n "Would you like to email this? [y/n] "
+    read ans
+    if [[ $ans =~ ^[Yy]$ ]]; then
+        echo -n "Your Email: "
+        read email
+        printf "${YELLOW}If you have 2 factor auth enabled, you need to generate an app specific password.\n"
+        printf "You can do so over at https://security.google.com/settings/security/apppasswords?pli=1 (assuming you use gmail)${RESET}\n"
+        echo -n "Password: "
+        read password
+        echo -n "Your Name: "
+        read name
+        echo -n "Recipient Email: "
+        read recipient
+        mail_attendance "$date" "$email" "$password" "$name" "$recipient"
+    fi
+}
+
+function mail_attendance() {
+    if [[ ! $(command -v mime-tool) ]]; then
+        printf "${YELLOW}mime-tool not detected. Attempting installation...${RESET}\n"
+        sudo apt-get install topal
+        if [[ ! $(command -v mime-tool) ]]; then
+            printf "${RED}mime-tool still not detected! Aborting!${RESET}\n"
+        fi
+    fi
+
+    printf "${YELLOW}Crafting email...${RESET}\n"
+    echo "From: \"$4\" <$2>" > message.txt
+    echo "Subject: Attendance for $date" >> message.txt
+    mime-tool "$date-Attendance.ods" >> message.txt
+    curl "smtps://smtp.gmail.com:465" --user "$2:$3" --mail-from "$2" --mail-rcpt "$5" --ssl --upload-file message.txt
+    rm message.txt $date-Attendance.csv $OUTPUT_FILE.csv
+}
+
 function help() {
     echo -e "Usage: ./scanner.sh [--offline|-h|--help]"
     echo -e " --offline\tTake attendance offline"
@@ -268,7 +360,8 @@ function main() {
         echo "8)  Delete attendance for a student on a particular day"
         echo "9)  Drop(delete) all attendance data"
         echo "10) Upload attendance from a log"
-        echo -e "11) Exit\n"
+        echo "11) Format attendance for a specific month (and option to email it)"
+        echo -e "12) Exit\n"
         printf "${GREEN}What would you like to do?>${RESET} "
         read choice
 
@@ -333,6 +426,10 @@ function main() {
             read log
             upload_attendance_from_log $log
         elif [[ $choice == "11" ]]; then
+            echo -n "Month to format? (1-12) "
+            read month
+            format_attendance $month
+        elif [[ $choice == "12" ]]; then
             printf "${RED}Exiting...${RESET}\n"
             exit
         fi
