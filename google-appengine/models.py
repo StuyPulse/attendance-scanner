f3aba00c7d70b8b1990dd102e3ce7543574e96fb
vs2961 <siuleunging@gmail.com>
2019-11-08 20:00:13 -0500

    Make flask secret key work

--- google-appengine/models.py
+++ google-appengine/models.py
@@ -35,5 +35,25 @@ class Student(ndb.Model):
 class Administrator(ndb.Model):
     password = ndb.StringProperty()
 
-class Settings(ndb.Expando):
-    pass
+class Settings(ndb.Model):
+  name = ndb.StringProperty()
+  value = ndb.StringProperty()
+
+  @staticmethod
+  def get(name):
+    NOT_SET_VALUE = "NOT SET"
+    client = ndb.Client()
+    with client.context() as context:
+        retval = Settings.query(Settings.name == name).get()
+        if not retval:
+          retval = Settings()
+          retval.name = name
+          retval.value = NOT_SET_VALUE
+          retval.put()
+        if retval.value == NOT_SET_VALUE:
+          raise Exception(('Setting %s not found in the database. A placeholder ' +
+            'record has been created. Go to the Developers Console for your app ' +
+            'in App Engine, look up the Settings record with name=%s and enter ' +
+            'its value in that record\'s value field.') % (name, name))
+        return retval.value
+
